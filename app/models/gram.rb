79365f3048a99a4dc5bfdbb4087fd9c1964024b1
melissa brown <melissabrown851@gmail.com>
2016-08-29 04:18:58 +0000

    Initial commit

---
+++ app/models/gram.rb
@@ -0,0 +1,8 @@
+class Gram < ActiveRecord::Base
+    validates :message, presence: true
+    validates :picture, presence: true
+    has_many :comments
+    
+    belongs_to :user
+    mount_uploader :picture, PictureUploader
+end
---
+++ spec/factories.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do 
+    
+    factory :user do 
+       sequence :email do |n|
+           "dummyEmail#{n}@gmail.com"
+       end
+       password "secretPassword"
+       password_confirmation "secretPassword"
+    
+    end
+    
+    
+    factory :gram do 
+        message "hello"
+		picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
+		association :user
+    end  
+end
